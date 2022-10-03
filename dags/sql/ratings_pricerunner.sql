8dea5c6c00142bd732ef658f89638dee90d57e1c
pergran1 <pergranberg@hotmail.com>
2022-10-03 23:40:27 +0200

    changed sql

--- dags/sql/ratings_pricerunner.sql
+++ dags/sql/ratings_pricerunner.sql
@@ -1,6 +1,6 @@
---enligt pricerunners template 
+
 select '{{ params.metric }}' as metric, 
-		rating as variable,   --pricerunner has country or category instead
+		rating as variable,
 		now() as timestamp, 
 		'{{ ds }}' as date, 
 		count(*) as value
